83ea2c42b85d0b236f441b4416d88b158edb902e
t-akuya <bankband1990@gmail.com>
2022-04-18 15:22:03 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'repertoires#index'
-  resources :repertoires, only: :index
+  resources :repertoires, only: [:index, :new, :create]
+
 end
