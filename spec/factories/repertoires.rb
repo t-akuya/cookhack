edfc6f34bb028c8d63ca685e7dc3f65459959708
t-akuya <bankband1990@gmail.com>
2022-04-20 13:38:56 +0900

    テストコード実装

--- spec/factories/repertoires.rb
+++ spec/factories/repertoires.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :repertoire do
-    
+    association :user
+    name                      {"料理名リョウリメイりょうりめい"}
+    cooking_time_id           {Faker::Number.between(from: 2, to: 10)}
+    recipe                    {"テストてすとtest"}
+    comment                   {"テストてすとtest"}
+    category_id               {Faker::Number.between(from: 2, to: 6)} 
+
+    after(:build) do |repertoire|
+      repertoire.image.attach(io: File.open('app/assets/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
