69c603a35c69f0b50695f8a25c231a80b970eafe
t-akuya <bankband1990@gmail.com>
2022-05-28 10:49:20 +0900

    動的フォーム実装の修正
    編集ページでも材料フォームの追加、削除の処理が行えるよう修正

--- app/javascript/packs/editAddForm.js
+++ app/javascript/packs/editAddForm.js
@@ -1,4 +1,5 @@
-let i = 1
+
+let i = 0
 
 //ページがロードされると...
 window.addEventListener("load", function () {
@@ -6,7 +7,6 @@ window.addEventListener("load", function () {
   //材料追加ボタンの要素をidから取得
   const addButton = document.getElementById("add-btn");
   let inputNum = document.getElementById("number").value;
-
   //取得したボタンをクリックすると...
   addButton.addEventListener("click", function() {
 
@@ -22,17 +22,17 @@ window.addEventListener("load", function () {
     const html = `<div id="ingredient-main">
                     <div id="ingredient-form">
                       <div id="add-form">
-                      <input placeholder="材料-${i}" type="text" 
-                        name="repertoire[ingredients_attributes][${inputNum}][name]" id="repertoire_ingredients_attributes_${inputNum}_name">
+                      <input placeholder="追加する材料${i}" type="text" 
+                      name="repertoire[ingredients_attributes][${inputNum}][name]" id="repertoire_ingredients_attributes_${inputNum}_name">
                       </div>
                       <div id="add-form">
-                      <input placeholder="分量-${i}" type="text" 
-                        name="repertoire[ingredients_attributes][${inputNum}][amount]" id="repertoire_ingredients_attributes_${inputNum}_amount">
+                      <input placeholder="追加する分量${i}" type="text" 
+                      name="repertoire[ingredients_attributes][${inputNum}][amount]" id="repertoire_ingredients_attributes_${inputNum}_amount">
                       </div>
                       <div id="add-form" class="delete" >
-                        <button type="button" id="repertoire_ingredients_attributes_${inputNum}_del" class="delete-btn" >
-                          削除
-                        </button>
+                        <button type="button" name="repertoire[ingredients_attributes][${inputNum}][del]" id="repertoire_ingredients_attributes_${inputNum}_del" class="delete-btn" >
+                        削除
+                      </button>
                       </div>
                     </div>
                   </div>`;
@@ -43,23 +43,30 @@ window.addEventListener("load", function () {
     //『(html)を複製(insertAdjacentHTML)し、(formList)の末尾(beforeend)に挿入する』
     formList.insertAdjacentHTML('beforeend', html);
 
+     //全ての削除ボタンを取得、deleteBtnsに定義する
+     const deleteBtns = document.querySelectorAll('.delete-btn')
+ 
+     //複数の削除ボタン(deleteBtns)をforEachで繰り返し処理,引数をbtnに設定
+     deleteBtns.forEach((btn) => {
+       //複数の削除ボタン=deleteBtns要素=(btn)をクリックすると引数del(64行目)の関数が発火
+       btn. addEventListener('click', del)
+    
+     })
 
-
-
-    //全ての削除ボタンを取得、deleteBtnsに定義する
-    //クリックイベントの関数内に記述することで追加ボタンを押下しないと削除ボタンが作動しないようにしている
+  })
+    
     const deleteBtns = document.querySelectorAll('.delete-btn')
 
-    //削除ボタンをforEachで繰り返し処理
     deleteBtns.forEach((btn) => {
-      //deleteBtns要素(btn)をクリックすると53行目sample関数が実行される
       btn. addEventListener('click', del)
     })
-  })
-  
-  //deleteBtnsのparentNode(親ノード)のさらにparentNode(親ノード)をremove(削除)する関数
-  function del() {
-    this.parentNode.parentNode.remove()
-  }
 
+    //関数delを定義
+    //deleteBtnsのparentNode(親ノード)のさらにparentNode(親ノード)をremove(削除)する関数
+    function del() {
+      console.log(this)
+      this.parentNode.parentNode.remove()
+    }
+  
+ 
 })
