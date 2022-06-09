d3432eca1ff7f1c9a936996553557cded0c30add
t-akuya <bankband1990@gmail.com>
2022-06-09 15:49:08 +0900

    モデル作成
    アソシエーション、バリデーション設定

--- app/models/cooking_hack.rb
+++ app/models/cooking_hack.rb
@@ -1,2 +1,12 @@
 class CookingHack < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :hack_image
+
+  with_options presence: true do
+    validates :title, length: {maximum: 40}
+    validates :contents
+    validates :hack_image
+  end
+
 end
