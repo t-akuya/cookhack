2501837baa5903fd6a89cac058bddd2c6b01bd06
t-akuya <bankband1990@gmail.com>
2022-06-11 09:42:14 +0900

    時短テクニック投稿単体テスト

--- spec/factories/cooking_hacks.rb
+++ spec/factories/cooking_hacks.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :cooking_hack do
+    association :user
+    
+    title             {"Hackタイトル"}
+    contents          {"時短テクニックの内容"}
+
+    after(:build) do |cooking_hack|
+      cooking_hack.hack_image.attach(io: File.open('app/assets/images/sample-hotcake.png'), filename: 'sample-hotcake.png')
+    end
     
   end
 end
--- spec/models/cooking_hack_spec.rb
+++ spec/models/cooking_hack_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe CookingHack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @cooking_hack = FactoryBot.build(:cooking_hack)
+    @cooking_hack.hack_image = fixture_file_upload("/test_image.png")
+  end
+
+  describe '時短テクニック投稿機能' do
+
+    context '投稿できるとき' do
+      it "項目が正しく入力されているとき" do
+        expect(@cooking_hack).to be_valid
+      end
+    end
+
+    context '投稿できないとき' do
+      it "画像ファイルが空では投稿できない" do
+        @cooking_hack.hack_image = nil
+        @cooking_hack.valid?
+        expect(@cooking_hack.errors.full_messages).to include("画像が入力されていません。")
+      end
+
+      it "タイトルが空では投稿できない" do
+        @cooking_hack.title = nil
+        @cooking_hack.valid?
+        expect(@cooking_hack.errors.full_messages).to include("Hackタイトルが入力されていません。")
+      end
+
+      it "タイトルが41文字以上では投稿できない" do
+        @cooking_hack.title = 'a' * 41
+        @cooking_hack.valid?
+        expect(@cooking_hack.errors.full_messages).to include("Hackタイトルは40文字以下に設定して下さい。")
+      end
+
+      it "時短テクニックの内容が空では投稿できない" do
+        @cooking_hack.contents = nil
+        @cooking_hack.valid?
+        expect(@cooking_hack.errors.full_messages).to include("時短テクニックの内容が入力されていません。")
+      end
+    end
+  end
+
 end
